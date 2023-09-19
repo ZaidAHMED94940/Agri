0475aa2ecdfd3d297c2cab7e486b7399eaae68dd
ZaidAHMED94940 <zaid94940@gmail.com>
2023-09-19 14:45:01 +0530

    first commit

--- client/myapp/src/Buyers.js
+++ client/myapp/src/Buyers.js
@@ -1,113 +1,66 @@
 import food from './images/food.jpg';
+import goodfood from './images/goodfood.png'
+import agrifood from './images/agrifood1.png'
+
 const Buyers = () => {
     return (
         <div className="Buyers">
             <div className="navabar1">
                 <h1 className="navh1">Welcome to Contact Page</h1>
             </div>
-            <div className="division">
-                <div className="mainBuyer">
-                <div className="contentbuyer">
-                    <h1>FoodFund International</h1>
-                    <h2>9203480234</h2>
-                    <h3>Street:  Apos; Mkt, Shop No.17, Sec 8, Phase Ii, Nr Mgm High School, Nerul</h3>
-                    <h3>City:Mumbai</h3>
-                    <h3>State/province/area:Maharashtra</h3>
-                    <h3>Zip code</h3>                     
-                    <h3>Country:India</h3>
+            <div className="Buyer">
+                <div className="buyercontent">
+                <h1>FoodFund International</h1>
+                <h2>9203480234</h2>
+                <h3>Street:  Apos; Mkt, Shop No.17, Sec 8, Phase Ii, Nr Mgm High School, Nerul</h3>
+                <h3>City:Mumbai</h3>
+                <h3>State/province/area:Maharashtra</h3>
+                <h3>Zip code</h3>                     
+                <h3>Country:India</h3>
                 </div>
-                <div className="imageer">
-                        <img id="foodimg1" src={food} alt="" />
-                        <button className="contactus">ContactUs</button>
-                    </div>
-                </div>
-                <div className="division">
-                <div className="mainBuyer">
-                <div className="contentbuyer">
-                    <h1>FoodFund International</h1>
-                    <h2>9203480234</h2>
-                    <h3>Street:  Apos; Mkt, Shop No.17, Sec 8, Phase Ii, Nr Mgm High School, Nerul</h3>
-                    <h3>City:Mumbai</h3>
-                    <h3>State/province/area:Maharashtra</h3>
-                    <h3>Zip code</h3>                     
-                    <h3>Country:India</h3>
-                </div>
-                <div className="imageer">
-                        <img id="foodimg1" src={food} alt="" />
-                        <button className="contactus">ContactUs</button>
-                    </div>
-                </div>
-                </div>
-                <div className="division">
-                <div className="mainBuyer">
-                <div className="contentbuyer">
-                    <h1>FoodFund International</h1>
-                    <h2>9203480234</h2>
-                    <h3>Street:  Apos; Mkt, Shop No.17, Sec 8, Phase Ii, Nr Mgm High School, Nerul</h3>
-                    <h3>City:Mumbai</h3>
-                    <h3>State/province/area:Maharashtra</h3>
-                    <h3>Zip code</h3>                     
-                    <h3>Country:India</h3>
-                </div>
-                <div className="imageer">
-                        <img id="foodimg1" src={food} alt="" />
-                        <button className="contactus">ContactUs</button>
-                    </div>
-                </div>
-                </div>
-                <div className="division">
-                <div className="mainBuyer">
-                <div className="contentbuyer">
-                    <h1>FoodFund International</h1>
-                    <h2>9203480234</h2>
-                    <h3>Street:  Apos; Mkt, Shop No.17, Sec 8, Phase Ii, Nr Mgm High School, Nerul</h3>
-                    <h3>City:Mumbai</h3>
-                    <h3>State/province/area:Maharashtra</h3>
-                    <h3>Zip code</h3>                     
-                    <h3>Country:India</h3>
-                </div>
-                <div className="imageer">
-                        <img id="foodimg1" src={food} alt="" />
-                        <button className="contactus">ContactUs</button>
-                    </div>
-                </div>
-                </div>
-                <div className="division">
-                <div className="mainBuyer">
-                <div className="contentbuyer">
-                    <h1>FoodFund International</h1>
-                    <h2>9203480234</h2>
-                    <h3>Street:  Apos; Mkt, Shop No.17, Sec 8, Phase Ii, Nr Mgm High School, Nerul</h3>
-                    <h3>City:Mumbai</h3>
-                    <h3>State/province/area:Maharashtra</h3>
-                    <h3>Zip code</h3>                     
-                    <h3>Country:India</h3>
-                </div>
-                <div className="imageer">
-                        <img id="foodimg1" src={food} alt="" />
-                        <button className="contactus">ContactUs</button>
-                    </div>
-                </div>
-                </div>
-                <div className="division">
-                <div className="mainBuyer">
-                <div className="contentbuyer">
-                    <h1>FoodFund International</h1>
-                    <h2>9203480234</h2>
-                    <h3>Street:  Apos; Mkt, Shop No.17, Sec 8, Phase Ii, Nr Mgm High School, Nerul</h3>
-                    <h3>City:Mumbai</h3>
-                    <h3>State/province/area:Maharashtra</h3>
-                    <h3>Zip code</h3>                     
-                    <h3>Country:India</h3>
-                </div>
-                <div className="imageer">
-                        <img id="foodimg1" src={food} alt="" />
-                        <button className="contactus">ContactUs</button>
-                    </div>
+                <div className="image">
+                    <img src={food} className="foodimg1"alt="logo" />
+                    <div>
+                    <button className="bit">Contact Us</button>
                 </div>
                 </div>
             </div>
+            <div className="Buyer">
+            <div className="buyercontent1">
+            <h1>The GoodFood Co</h1>
+            <h2>9203480234</h2>
+            <h3>Street:  Apos; Mkt, Shop No.17, Sec 8, Phase Ii, Nr Mgm High School, Nerul</h3>
+            <h3>City:Mumbai</h3>
+            <h3>State/province/area:Maharashtra</h3>
+            <h3>Zip code</h3>                     
+            <h3>Country:India</h3>
+            </div>
+            <div className="image1">
+                <img src={goodfood} id="goodfood"className="foodimg1"alt="logo" />
+                <div className="button12">
+                <button className="bit">Contact Us</button>
+            </div>
+            </div>
         </div>
+        <div className="Buyer">
+            <div className="buyercontent1">
+            <h1>The GoodFood Co</h1>
+            <h2>9203480234</h2>
+            <h3>Street:  Apos; Mkt, Shop No.17, Sec 8, Phase Ii, Nr Mgm High School, Nerul</h3>
+            <h3>City:Mumbai</h3>
+            <h3>State/province/area:Maharashtra</h3>
+            <h3>Zip code</h3>                     
+            <h3>Country:India</h3>
+            </div>
+            <div className="image1">
+                <img src={agrifood} id="goodfood"className="foodimg1"alt="logo" />
+                <div className="button12">
+                <button className="bit">Contact Us</button>
+            </div>
+            </div>
+        </div>
+        </div>
+
     )
 }
 
--- client/myapp/src/Products.js
+++ client/myapp/src/Products.js
@@ -20,7 +20,7 @@ const Products = () => {
                 <h2 className="welc">Welcome to Agrigrow</h2></div>
             <div className="cardgrid">
                 <div class="card">
-                    <img id="onion" src={onion} alt="Card image cap" />
+                    <img id="onion" src={onion}  />
                     <div class="card-body">
                         <h3 class="card-title">Onion</h3>
                         <h4>RS 35 PER 1KG</h4>
@@ -28,7 +28,7 @@ const Products = () => {
                     </div>
                 </div>
                 <div class="card">
-                    <img id="potato" src={potato} alt="Card image cap" />
+                    <img id="potato" src={potato}  />
                     <div class="card-body">
                         <h3 class="card-title">Potato</h3>
                         <h4>RS 30 PER KG</h4>
@@ -36,7 +36,7 @@ const Products = () => {
                     </div>
                 </div>
                 <div class="card">
-                    <img id="onion" src={ladyfinger} alt="Card image cap" />
+                    <img id="onion" src={ladyfinger}  />
                     <div class="card-body">
                         <h3 class="card-title">Ladyfinger</h3>
                         <h4>RS 90 PER KG</h4>
@@ -44,7 +44,7 @@ const Products = () => {
                     </div>
                 </div>
                 <div class="card">
-                    <img id="cauliflower" src={cauliflower} alt="Card image cap" />
+                    <img id="cauliflower" src={cauliflower}  />
                     <div class="card-body">
                         <h3 class="card-title">Cauliflower</h3>
                         <h4>RS 80 PER KG</h4>
@@ -54,7 +54,7 @@ const Products = () => {
             </div>
             <div className="cardgrid">
                 <div class="card">
-                    <img id="potato" src={tomatoes} alt="Card image cap" />
+                    <img id="potato" src={tomatoes} />
                     <div class="card-body">
                         <h3 class="card-title">Tomatoes</h3>
                         <h4>RS 150 PER KG</h4>
@@ -62,7 +62,7 @@ const Products = () => {
                     </div>
                 </div>
                 <div class="card">
-                    <img id="onion" src={brinjal} alt="Card image cap" />
+                    <img id="onion" src={brinjal}  />
                     <div class="card-body">
                         <h3 class="card-title">Brinjal</h3>
                         <h4>RS 120 PER KG</h4>
@@ -70,7 +70,7 @@ const Products = () => {
                     </div>
                 </div>
                 <div class="card">
-                    <img id="onion" src={cucumber} alt="Card image cap" />
+                    <img id="onion" src={cucumber}  />
                     <div class="card-body">
                         <h3 class="card-title">Cucumber</h3>
                         <h4>RS 100 PER KG</h4>
@@ -78,7 +78,7 @@ const Products = () => {
                     </div>
                 </div>
                 <div class="card">
-                    <img id="onion" src={cabbage} alt="Card image cap" />
+                    <img id="onion" src={cabbage}  />
                     <div class="card-body">
                         <h3 class="card-title">Cabbage</h3>
                         <h4>RS 140 PER KG</h4>
@@ -88,7 +88,7 @@ const Products = () => {
             </div>
             <div className="cardgrid">
                 <div class="card">
-                    <img id="onion" src={carrot} alt="Card image cap" />
+                    <img id="onion" src={carrot}  />
                     <div class="card-body">
                         <h3 class="card-title">Carrot</h3>
                         <h4>RS 70 PER KG</h4>
@@ -96,7 +96,7 @@ const Products = () => {
                     </div>
                 </div>
                 <div class="card">
-                    <img id="potato" src={chillies} alt="Card image cap" />
+                    <img id="potato" src={chillies}  />
                     <div class="card-body">
                         <h3 class="card-title">Green Chillie</h3>
                         <h4>RS 130 PER KG</h4>
@@ -104,7 +104,7 @@ const Products = () => {
                     </div>
                 </div>
                 <div class="card">
-                    <img id="onion" src={corn} alt="Card image cap" />
+                    <img id="onion" src={corn}  />
                     <div class="card-body">
                         <h3 class="card-title">Corn</h3>
                         <h4>RS 200 PER KG</h4>
@@ -112,7 +112,7 @@ const Products = () => {
                     </div>
                 </div>
                 <div class="card">
-                    <img id="potato" src={Peas} alt="Card image cap" />
+                    <img id="potato" src={Peas}  />
                     <div class="card-body">
                         <h3 class="card-title">Peas</h3>
                         <h4>RS 200 PER KG</h4>
